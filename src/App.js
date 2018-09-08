4ec32163fcef649232e3fa7e01d1e7832ddaf70d
Rodrigo Alejandro Juarez <rodrigo.juarez.inf@gmail.com>
2018-09-08 20:24:28 +0200

    Init

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="box">1</div>
+        <div className="box box2">2</div>
+        <div className="box">3</div>
+        <div className="box">4</div>
+        <div className="box">5</div>
+        <div className="box">6</div>
+        <div className="box">7</div>
+        <div className="box">8</div>
+        <div className="box">9</div>
+        <div className="box">10</div>
+        <div className="box">11</div>
+        <div className="box">12</div>
+      </div>
+    );
+  }
+}
+
+export default App;
