0c0ed58088ce4eb777ddb5f3f488df6038605627
Rodrigo Alejandro Juarez <rodrigo.juarez.inf@gmail.com>
2018-09-09 08:06:21 +0200

    More layout and utility classes

--- src/App.js
+++ src/App.js
@@ -5,25 +5,32 @@ class App extends Component {
   render() {
     return (
       <div className="wrapper">
-        <div className="box d-flex">
-          <div>
-            <p class="text-small">Hello</p>
+        <div className="new d-flex">
+          <div class="d-flex flex-column ai-center px-1">
+            <i className="arrow-up" />
+            <p className="text-small mt-0">10</p>
           </div>
           <div>
-            <p class="text-small">Bye</p>
+            <p className="text-bold text-small my-0">
+              My YC app: Dropbox - Throw away your USB drive
+            </p>
+            <p className="text-light text-small mt-0">By dhouston</p>
+            <p className="text-small">
+              Aw shuck, guys you make me blush with your compliments
+            </p>
           </div>
         </div>
-        <div className="box box2">2</div>
-        <div className="box">3</div>
-        <div className="box">4</div>
-        <div className="box">5</div>
-        <div className="box">6</div>
-        <div className="box box2">7</div>
-        <div className="box">8</div>
-        <div className="box">9</div>
-        <div className="box">10</div>
-        <div className="box">11</div>
-        <div className="box">12</div>
+        <div className="new">2</div>
+        <div className="new">3</div>
+        <div className="new">4</div>
+        <div className="new">5</div>
+        <div className="new">6</div>
+        <div className="new">7</div>
+        <div className="new">8</div>
+        <div className="new active">9</div>
+        <div className="new">10</div>
+        <div className="new">11</div>
+        <div className="new">12</div>
       </div>
     );
   }
