7a3c0c5fadb701877abf313181809b495cdfb966
Rodrigo Alejandro Juarez <rodrigo.juarez.inf@gmail.com>
2018-09-09 07:41:03 +0200

    Basic layout

--- src/App.js
+++ src/App.js
@@ -5,13 +5,20 @@ class App extends Component {
   render() {
     return (
       <div className="wrapper">
-        <div className="box">1</div>
+        <div className="box d-flex">
+          <div>
+            <p class="text-small">Hello</p>
+          </div>
+          <div>
+            <p class="text-small">Bye</p>
+          </div>
+        </div>
         <div className="box box2">2</div>
         <div className="box">3</div>
         <div className="box">4</div>
         <div className="box">5</div>
         <div className="box">6</div>
-        <div className="box">7</div>
+        <div className="box box2">7</div>
         <div className="box">8</div>
         <div className="box">9</div>
         <div className="box">10</div>
