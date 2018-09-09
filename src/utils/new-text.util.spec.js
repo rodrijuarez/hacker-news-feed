fa218c95459281c13a046d6cadcf3867e2e000c6
Rodrigo Alejandro Juarez <rodrigo.juarez.inf@gmail.com>
2018-09-09 08:53:09 +0200

    Mock data

--- src/utils/new-text.util.spec.js
+++ src/utils/new-text.util.spec.js
@@ -1,5 +1,17 @@
 import {buildSubtitle} from './new-text.util';
 
+const story = {
+  by: 'rodrigo',
+  descendants: 10,
+  id: 17943030,
+  kids: [17944537, 17944200, 17943139],
+  score: 28,
+  time: 1536444088,
+  title: 'USB drives with no phantom load',
+  type: 'story',
+  url: 'http://joeyh.name/blog/entry/usb_drives_with_no_phantom_load/',
+};
+
 expect('new text util', () => {
   it('should have a function for building the sub-title', () => {
     expect(newSubtitle(story)).toBe('By Rodrigo');
