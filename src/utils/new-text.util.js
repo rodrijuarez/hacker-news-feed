05813e5a11a29153e4b55256969dc498dd839c16
Rodrigo Alejandro Juarez <rodrigo.juarez.inf@gmail.com>
2018-09-09 09:06:59 +0200

    Fix on uppercase first letter

--- src/utils/new-text.util.js
+++ src/utils/new-text.util.js
@@ -1,6 +1,9 @@
 import * as R from 'ramda';
 
-const upperCaseFirstLetter = R.over(R.head, R.toUpper);
+const upperCaseFirstLetter = R.compose(
+  R.join(''),
+  R.over(R.lensIndex(0), R.toUpper),
+);
 
 export const buildSubtitle = R.compose(
   R.concat('By '),
