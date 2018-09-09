32ac72c13666427aff4a42276ac11ee38e178504
Rodrigo Alejandro Juarez <rodrigo.juarez.inf@gmail.com>
2018-09-09 10:10:24 +0200

    Added extra test

--- src/utils/active-stories.util.test.js
+++ src/utils/active-stories.util.test.js
@@ -12,10 +12,26 @@ const story = {
   url: 'http://joeyh.name/blog/entry/usb_drives_with_no_phantom_load/',
 };
 
+const storyTwo = {
+  by: 'rodrigo',
+  descendants: 10,
+  id: 17943031,
+  kids: [17944537, 17944200, 17943139],
+  score: 28,
+  time: 1536444088,
+  title: 'SSD Drive dual boot (Linux & Windows)',
+  type: 'story',
+  url: 'http://joeyh.name/blog/entry/usb_drives_with_no_phantom_load/',
+};
+
 it('should be able to toggle active stories into inactive', () => {
   expect(toggleStories([story], [story])).toEqual([]);
 });
 
 it('should be able to toggle active stories into inactive', () => {
   expect(toggleStories([], [story])).toEqual([story]);
+});
+
+it('should not toggle all the stories, only the ones received in the second param', () => {
+  expect(toggleStories([story, storyTwo], [story])).toEqual([storyTwo]);
 });
